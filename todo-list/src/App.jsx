ee7f2e4c9ae6a3d4b01628d37b59d4939e3c79c6
matiasvincent2002 <72351361+matiasvincent2002@users.noreply.github.com>
2023-11-04 21:40:17 -0300

    first commit

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,21 @@
+// src/App.jsx
+
+import React from 'react';
+import TodoList from '../src/component/Todo-list/TodoList.jsx';
+import reactLogo from './assets/react.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+      
+
+      <TodoList /> {/* Render the TodoList component */}
+ 
+    </>
+  );
+}
+
+export default App;
